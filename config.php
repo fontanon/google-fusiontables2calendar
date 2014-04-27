9550898dd7fdd1644e80b76584b536103ba0138f
J. Félix Ontañón <fontanon@gmail.com>
2014-04-27 19:32:29 +0200

    Document strings for config vars

--- config.php
+++ config.php
@@ -1,11 +1,28 @@
 <?php 
+
+	// ::auth config::
+	// Google Public API Access Key. Don't forget to activate Google API for fusion tables
+	// Get one and configure access and perms at https://console.developers.google.com
 	$API_KEY="add yours";
 
+	// ::fusion tables config::
+	// The Fusion Tables ID (find it at fusion tables URL: the docid param)
 	$TABLE_ID="add yours";
-	$COL_NAMES="add yours";
+
+	// Name of columns to retrieve from the fusion table, separated by ','.
+	// Below ones are mandatory for building the calendar, and its position sensitive.
+	$COL_NAMES="summary,description,start,end,location,url";
+
+	// Date format in the fusion table. Examples: d/M/Y, Y-M-d H:i:s, YMdTHisZ ...
 	$DATE_FORMAT="add yours";
 
+	// ::calendar config::
+	// Name of your calendar
 	$CAL_NAME="add yours";
+
+	// Description of your calendar
 	$CAL_DESC="add yours";
+
+	// Timezone of your calendar. Examples: US/Pacific, Europe/Madrid
 	$CAL_TZ="add yours";
 ?>
